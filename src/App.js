342280e832ed750f08930614b50ff537310c0086
Adam Goth <adamgoth@gmail.com>
2017-09-17 17:32:22 -0600

    Removes unnecessary reference

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 import CurrentPrices from './components/CurrentPrices';
 import PriceHistory from './components/PriceHistory';
 
